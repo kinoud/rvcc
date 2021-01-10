29073322633c4482a3cda9ed165527b038ac3035
kinoud <kinoud@qq.com>
2021-01-10 14:57:10 +0800

    symtab

--- icg/symtab.py
+++ icg/symtab.py
@@ -1,82 +1,9 @@
 from pycparser import c_ast
 from pycparser import CParser
 
+from symbol import *
+from symbol import FuncSymbol
 import argparse
-
-class OFFSET():
-    GLOBAL = 'global' # 全局偏移 (一个c源文件中的全局变量)
-    LOCAL = 'local' # 局部偏移 (局部变量\struct成员变量)
-
-class Symbol():
-    def __init__(self, name, size=0, offset=0, offset_type=OFFSET.GLOBAL, type_str=None):
-        self.name = name
-        self.size = size # 以byte计
-        self.offset = offset # 以byte计
-        self.offset_type = offset_type # 取值为OFFSET中的常量
-        self.type = type_str
-        # self.Reg=None
-        # self.isChange=False
-        # self.inMem=False
-        self.isTmp = False
-        self.isConst = False
-        self.val = 0 # isConst 为 True 才有意义
-
-    def __str__(self):
-        return repr(self)
-    def __repr__(self):
-        return '\033[1;33m%s\033[0m,sz=%d,off=%d(%s),type=%s'%(
-            self.name,self.size,self.offset,self.offset_type,self.type)
-
-class BasicSymbol(Symbol):
-    '''
-    基本类型 int short long bool char 等
-    '''
-    SIZE_OF = {
-        'long long':8,
-        'long long int':8,
-        'long':4,
-        'long int':4,
-        'signed':4,
-        'unsigned':4,
-        'int':4,
-        'short int':2,
-        'short':2,
-        'char':1,
-        'bool':1
-    }
-    
-    @staticmethod
-    def gen_symbol(name:str, type_str:str):
-        '''
-        若type_str描述的不是一个基本类型则返回None
-        '''
-        try:
-            return BasicSymbol(name, type_str)
-        except NotImplementedError:
-            return None
-
-    def __init__(self, name, type_str:str):
-        super().__init__(name)
-        a = type_str.split(' ')
-        self.unsigned = False
-        if a[0] == 'unsigned':
-            self.unsigned = True
-            if len(a) > 1:
-                a=a[1:]
-        a = ' '.join(a)
-        szof = BasicSymbol.SIZE_OF
-        if szof.get(a) is None:
-            raise NotImplementedError('类型"'+a+'"尚不支持')
-        self.size = szof[a]
-        self.type = a
-
-    def __repr__(self):
-        ans = super().__repr__()
-        if self.unsigned:
-            ans+='(u)'
-        return ans
-
-    
 
 class SymTab():
     def __init__(self, node:c_ast.Node, parent=None):
@@ -87,6 +14,7 @@ class SymTab():
         self.children=[]
         self.syms = {}
         self.tmps = {}
+        self.types = {}
 
     def get_symbol(self, name:str) -> Symbol:
         local_res = self.syms.get(name)
@@ -99,6 +27,19 @@ class SymTab():
     def add_symbol(self, sym:Symbol):
         self.syms[sym.name] = sym
 
+    def get_type(self, name:str) -> Type:
+        x = self.types.get(name)
+        if x is not None:
+            return x
+        if self.parent is None:
+            return None
+        return self.parent.get_type(name)
+
+    def add_type(self, typ:Type, alias:str = None) -> Type:
+        if alias is None:
+            alias = typ.name
+        self.types[alias] = typ
+
     def __repr__(self):
         ans = type(self.node).__name__ 
         len_name = len(ans)
@@ -108,6 +49,8 @@ class SymTab():
             ans += ' ['+repr(x)+']'
         for x in self.tmps.values():
             ans += '\n' + ' '*len_name + ' (tmp)['+repr(x)+']'
+        for k,v in self.types.items():
+            ans += '\n' + ' '*len_name + ' (type '+name+')['+repr(v)+']'
         return ans
 
     def __iter__(self):
@@ -130,118 +73,6 @@ class SymTab():
         newTmp.isTmp = True
         self.tmps[varName] = newTmp
         return newTmp
-
-
-class StructSymbol(Symbol):
-    '''
-    结构体符号,特别之处是它自身持有一张符号表,记录成员变量信息.
-    size: 结构体的总大小
-    member_symtab: 成员变量符号表
-    '''
-    def __init__(self, name, **kwarg):
-        super().__init__(name, offset_type=OFFSET.LOCAL, type_str='struct', **kwarg)
-        self.member_symtab = SymTab(None)
-        self.element_paths = []
-        self.atoms = 0
-
-    def get_member_symbol(self, name:str) -> Symbol:
-        return self.member_symtab.get_symbol(name)
-    
-    def add_member_symbol(self, sym:Symbol):
-        self.member_symtab.add_symbol(sym)
-
-    def __repr__(self):
-        ans = super().__repr__()
-        members=''
-        for name in self.member_symtab.keys():
-            members += name+','
-        ans+=',members=['+members[:-1]+']'
-        return ans
-
-class FuncSymbol(Symbol):
-    '''
-    函数符号
-    size: 返回值的size
-    params_symtab: 参数符号表
-    frame_size: (返回值+参数列表+所有局部变量)的总大小,以byte计
-    local_symbols: list, 存储了所有局部变量的symbol
-    '''
-    def __init__(self, name):
-        super().__init__(name)
-        self.params_symtab = SymTab(None)
-        self.frame_size = None
-        self.local_symbols = []
-
-    def add_param_symbol(self, sym:Symbol):
-        self.params_symtab.add_symbol(sym)
-
-    def __repr__(self):
-        ans = super().__repr__()
-        params=''
-        for name in self.params_symtab.syms.keys():
-            params += name+','
-        ans+=',params=['+params[:-1]+'],frame_sz='+str(self.frame_size)
-        locals=''
-        for sym in self.local_symbols:
-            locals += sym.name+','
-        ans+=',locals=['+locals[:-1]+']'
-        return ans
-
-class PointerSymbol(Symbol):
-    '''
-    指针符号
-    size: 指针的大小,所有指针的大小都是固定的4byte
-    target_size: 最终指向的元素的大小
-    target_type: 最终指向的元素的类型名,str类型
-    level: 指针级数 从1开始
-    '''
-    def __init__(self, name, target_size, target_type=None, level=1, **kwarg):
-        super().__init__(name, size=4, type_str='pointer', **kwarg)
-        self.target_size = target_size
-        self.target_type = target_type
-        self.level = level
-    
-    def __repr__(self):
-        ans = super().__repr__()
-        ans += '(%d),t_sz=%d,t_type=%s'%(self.level,self.target_size,self.target_type)
-        return ans
-
-class ArraySymbol(Symbol):
-    '''
-    数组符号
-    size: 数组的总大小
-    element_type: 基本元素的类型
-    dims: list, 维度列表dim[i]是从左到右第i维
-    '''
-    def __init__(self, name, element_type, dims, **kwarg):
-        super().__init__(name, type_str = 'array', **kwarg)
-        self.element_type = element_type
-        self.dims = dims
-
-        self.ks = [1]
-        for i in range(len(self.dims)-1, 0, -1):
-            self.ks.append(self.ks[-1] * self.dims[i])
-        self.len = 1
-        for item in self.dims:
-            self.len *= item
-
-        if element_type in BasicSymbol.SIZE_OF.keys():
-            self.element_size = BasicSymbol.SIZE_OF[element_type]
-        else:
-            self.element_size = int(self.size / self.len)
-
-    def __repr__(self):
-        total = 1
-        dims_str=''
-        for d in self.dims:
-            total *= d
-            dims_str += '%d,'%d
-        dims_str = '['+dims_str[:-1]+']'
-        ans = super().__repr__()+',e_sz=%d,e_type=%s,dims=%s'%(
-            self.size//total,self.element_type,dims_str)
-        return ans
-
-    
 class SymTabStore():
     def __init__(self):
         self._symtab={} # dict[c_ast.Node]->SymTab
@@ -298,7 +129,7 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
     # 它的内容总是当前遍历过程中正在处理的节点的符号表
 
 
-    def dfs(u:c_ast.Node):
+    def dfs(u:c_ast.Node, **kwargs):
         '''
         此函数将根据u的类型为它选择相应的具体的dfs函数去处理,
         与此同时维护符号表的建立与current_symtab的值
@@ -321,7 +152,7 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
         saved_symtab = current_symtab
         current_symtab = t
 
-        res = dfs_fn(u)
+        res = dfs_fn(u, **kwargs)
 
         current_symtab = saved_symtab
 
@@ -340,8 +171,8 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
             res = dfs(v)
             if res.get('symbol') is not None:
                 t.add_symbol(res['symbol'])
-            if res.get('struct_symbol') is not None:
-                t.add_symbol(res['struct_symbol'])
+            if res.get('struct_type') is not None:
+                t.add_type(res['struct_type'])
     
     @register('Decl')
     def decl(u:c_ast.Decl) -> dict:
@@ -363,17 +194,11 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
 
         type_name = type(u.type).__name__
         if type_name == 'FuncDecl':
-            res = dfs(u.type)
-            res['func_symbol'].name = u.name
-            return res
+            return {'symbol':dfs(u.type, name=u.name)} # return FuncSymbol
 
         if type_name == 'PtrDecl':
-            res = dfs(u.type)
-            x = PointerSymbol(u.name, res['target_size'], res['target_type'],level=res['level'],
-                offset=offset, offset_type=get_offset_type()
-            )
-            offset += x.size
-            return {'symbol':x}
+            ptr_type = dfs(u.type)
+            return {'symbol': ptr_type.gen_symbol(u.name)}
 
         if type_name == 'ArrayDecl':
             res = dfs(u.type)
@@ -386,28 +211,22 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
             offset += x.size
             return {'symbol':x}
 
-        res = dfs(u.type)
-
-        x = None
+        t:Type = dfs(u.type) # u.type: Struct TypeDecl
 
         # 仅定义结构体而不声明变量则name=None
-        if u.name is not None:
-            x = BasicSymbol.gen_symbol(u.name, res['type'])
-            if x is None:
-                x = Symbol(u.name, size=res['size'], type_str=res['type'])
-            x.offset = offset
-            offset += x.size
-            x.offset_type = get_offset_type()
+        if u.name is None:
+            x = None
+        else:
+            x = t.gen_symbol(u.name)
 
-        struct_symbol = None
-        if res.get('struct_symbol') is not None:
-            struct_symbol = res['struct_symbol']
-        
+        struct_type = None
+        if isinstance(t, StructType):
+            struct_type = t
 
-        return {'symbol':x, 'struct_symbol':struct_symbol}
+        return {'symbol':x, 'struct_type':struct_type}
 
     @register('Struct')
-    def struct(u:c_ast.Struct):
+    def struct(u:c_ast.Struct)->Type:
         '''
         语法树中出现Struct有两种情况(暂时发现两种),
         一是定义一个struct,二是使用一个struct.
@@ -419,22 +238,17 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
 
         # 使用一个struct,而不是定义它
         t = sts.get_symtab_of(u)
-        type_str = 'struct '+u.name
+
         if u.decls is None:
-            sym = t.get_symbol(type_str)
-            return {'size':sym.size,'type':type_str}
+            return t.get_type(u.name) # u.name 是struct的名字(不含'struct')
 
         # 定义一个struct
-        saved_offset = offset
-        offset = 0 # struct内部成员变量的offset是独立的,和函数一样,因而先置零
-        in_struct = True
-
-        size = 0
         struct_symbol = StructSymbol(type_str)
         for d in u.decls:
-            res = dfs(d) 
-            if res.get('struct_symbol') is not None:
+            _,typ = dfs(d) 
+            if typ is not None:
                 t.add_symbol(res['struct_symbol'])
+                t.add
             x = res['symbol']
             x.offset_type = OFFSET.LOCAL
             struct_symbol.add_member_symbol(x)
@@ -448,69 +262,30 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
         return {'size':size, 'struct_symbol':struct_symbol, 'type':type_str}
 
     @register('TypeDecl')
-    def type_decl(u:c_ast.TypeDecl) -> dict:
-        return dfs(u.type)
+    def type_decl(u:c_ast.TypeDecl) -> Type:
+        return dfs(u.type) # IdentifierType Struct
 
     @register('IdentifierType')
-    def identifier_type(u:c_ast.IdentifierType) -> dict:
-        '''
-        return:
-            size: 符号的size,以byte计
-            type: 符号的type(str),不忽略unsigned
-        '''
+    def identifier_type(u:c_ast.IdentifierType) -> Type:
+        # 暂时认为identifier_type都是BasicType
         type_str = ' '.join(u.names)
-
-        x = BasicSymbol.gen_symbol('test',type_str)
-
-        if x is None:
-            raise NotImplementedError('类型"'+type_str+'"尚不支持')
-
-        return {'size' : x.size, 'type' : type_str}
+        return BasicType(type_str)
 
     @register('FuncDecl')
-    def func_decl(u:c_ast.FuncDecl):
-        nonlocal offset
-        func_symbol = FuncSymbol(None)
-        func_symbol.offset = offset
-        res = dfs(u.type)
-        func_symbol.size = res['size']
-        func_symbol.type = 'function'
+    def func_decl(u:c_ast.FuncDecl, name:str):
+        param_symbols = dfs(u.args) # to ParamList
+        return_type = dfs(u.type) # to TypeDecl
+        func_type = FuncType(return_type, [p.type for p in param_symbols])
+        return FuncSymbol(name, func_type, [p.name for p in param_symbols])
         
-
-        # x: 返回值的symbol
-
-        type_name = type(u.type).__name__
-        if type_name == 'PtrDecl':
-            # 指针类型
-            x = PointerSymbol('__ret__', res['target_size'], res['target_type'],level=res['level'],
-                offset=offset, offset_type=get_offset_type()
-            )
-        else:
-            # basic类型或者struct类型
-            x = BasicSymbol.gen_symbol('__ret__', res['type'])
-            if x is None:
-                x = Symbol('__ret__', size=res['size'], type_str=res['type'],
-                    offset=offset, offset_type=get_offset_type() 
-                )
-        offset += x.size
-
-        # x end
-
-        res = dfs(u.args)
-        symbols=[]
-        if res.get('param_symbols') is not None:
-            symbols = res['param_symbols']
-        size = dfs(u.type)['size']
-        return {'func_symbol':func_symbol,'param_symbols':symbols,'size':size , 'return_symbol':x}
 
     @register('ParamList')
     def paramlist(u:c_ast.ParamList):
         symbols = []
         for d in u.params: # d是Decl类型
             x = dfs(d)['symbol']
-            x.offset_type = OFFSET.LOCAL
             symbols.append(x)
-        return {'param_symbols':symbols}
+        return symbols
     
     # unsupported
     # @register('EllipsisParam')
@@ -531,13 +306,8 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
 
     @register('FuncDef')
     def func_def(u:c_ast.FuncDef):
-        nonlocal in_func, offset, sts
-        in_func = True
-        saved_offset = offset
-        offset = 0
-
+        nonlocal sts
         t = sts.get_symtab_of(u)
-
         '''
         FuncDef的第一个孩子是Decl.
         在Decl的dfs处理函数中,我们生成一个符号,并设置该符号的offset和size.
@@ -545,33 +315,14 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
         的符号,其size是返回值类型的size,offset是0.
         于是,我们约定在函数内部,返回值的offset为0.
         '''
-        res = dfs(u.decl)
-        x = res['func_symbol'] # 代表此函数本身的符号,类型是FuncSymbol
-        
-        for sym in [res['return_symbol']] + res['param_symbols']: # 此函数的参数的符号
-            t.add_symbol(sym)
-            x.add_param_symbol(sym)
+        x = dfs(u.decl)['symbol'] # FuncSymbol
 
-        
+        for sym in x.param_symbols:
+            t.add_symbol(sym)
+        t.add_symbol(x.return_symbol)
 
         dfs(u.body)
 
-        local_symbols = []
-        def local_dfs(u:c_ast.Node):
-            t = sts.get_symtab_of(u)
-            if t is None:
-                return 
-            for sym in t:
-                local_symbols.append(sym)
-            for v_name, v in u.children():
-                local_dfs(v)
-        local_dfs(u.body)
-        x.local_symbols = local_symbols
-
-        x.frame_size = offset
-        
-        offset = saved_offset
-        in_func = False
         return {'symbol':x}
 
 
@@ -582,25 +333,9 @@ def symtab_store(ast:c_ast.Node) -> SymTabStore:
         dfs(u.iffalse)
 
     @register('PtrDecl')
-    def ptr_decl(u:c_ast.PtrDecl):
-        '''
-        返回
-        target_size: 指向元素的size
-        target_type: 指向元素的type
-        size: 4(指针的大小)
-        type: 'pointer'
-        '''
-        res = dfs(u.type)
-        if res.get('target_size') is not None: # 说明下一级仍然是指针
-            target_size = res['target_size']
-            target_type = res['target_type']
-            level = res['level'] + 1
-        else:
-            target_size = res['size']
-            target_type = res['type']
-            level = 1
-
-        return {'target_size': target_size,'target_type': target_type,'size':4,'type':'pointer','level':level}
+    def ptr_decl(u:c_ast.PtrDecl) -> PtrType:
+        target_type = dfs(u.type)
+        return PtrType(target_type)
         
     @register('ArrayDecl')
     def array_decl(u:c_ast.ArrayDecl):
@@ -744,5 +479,6 @@ if __name__=='__main__':
     parser = CParser()
     with open(args.filename,'r') as f:
         ast = parser.parse(f.read(), args.filename)
+    print(ast)
     sts = symtab_store(ast)
     sts.show(ast)
