668cb21670a13d9fbac5855d8b70e6620d40a655
kinoud <1301521455@qq.com>
2021-01-16 17:16:36 +0800

    ide

--- ide.py
+++ ide.py
@@ -42,58 +42,109 @@ def get_output(fn,args):
     p.join()
     return q.get()
     
-class TextEditDemo(QWidget):
+class CompilerPage(QWidget):
   def __init__(self,parent=None):
-    super(TextEditDemo, self).__init__(parent)
+    super(CompilerPage, self).__init__(parent)
     self.setWindowTitle('SEU RISCV CC')
  
     #定义窗口的初始大小
-    self.resize(800,600)
+    self.resize(1200,600)
     #创建多行文本框
-    self.textEdit=QTextEdit()
-    self.textEdit.textChanged.connect(self.on_text_change)
-    self.textShow=QTextEdit()
-    self.textShow.setFontFamily('consolas')
-    self.textShow.setFontPointSize(13)
+    self.textSrc = QTextEdit()
+    self.textAsm = QTextEdit()
+    self.textCoe = QTextEdit()
+    self.btnGo=QPushButton('Go')
+    self.btnAsm=QPushButton('Assemble')
+    self.btnSaveAsm=QPushButton('Save ASM')
+    self.btnSaveCoe=QPushButton('Save COE')
 
 
+    self.cpSrc = QWidget()
+    layout = QHBoxLayout()
+    layout.addStretch(1)
+    layout.addWidget(self.btnGo)
+    self.cpSrc.setLayout(layout)
 
-    #创建两个按钮
-    self.btnPress1=QPushButton('生成汇编')
-    self.btnPress2=QPushButton('显示HTML')
+    self.cpAsm = QWidget()
+    layout = QHBoxLayout()
+    layout.addStretch(1)
+    layout.addWidget(self.btnAsm)
+    layout.addWidget(self.btnSaveAsm)
+    self.cpAsm.setLayout(layout)
 
-    self.controlPanel = QWidget()
+    self.cpCoe = QWidget()
+    layout = QHBoxLayout()
+    layout.addStretch(1)
+    layout.addWidget(self.btnSaveCoe)
+    self.cpCoe.setLayout(layout)
+
+
+    self.sliceSrc = QWidget()
     layout = QVBoxLayout()
-    layout.addWidget(self.btnPress1)
-    layout.addWidget(self.btnPress2)
-    self.controlPanel.setLayout(layout)
+    layout.addWidget(self.textSrc)
+    layout.addWidget(self.cpSrc)
+    self.sliceSrc.setLayout(layout)
+
+    self.sliceAsm = QWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(self.textAsm)
+    layout.addWidget(self.cpAsm)
+    self.sliceAsm.setLayout(layout)
+
+    self.sliceCoe = QWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(self.textCoe)
+    layout.addWidget(self.cpCoe)
+    self.sliceCoe.setLayout(layout)
  
     layout=QHBoxLayout()
-    layout.addWidget(self.textEdit)
-    layout.addWidget(self.textShow)
-    layout.addWidget(self.controlPanel)
+    layout.addWidget(self.sliceSrc)
+    layout.addWidget(self.sliceAsm)
+    layout.addWidget(self.sliceCoe)
     self.setLayout(layout)
  
-    self.btnPress1.clicked.connect(self.btnPress1_clicked)
-    self.btnPress2.clicked.connect(self.btnPress2_clicked)
+    self.btnGo.clicked.connect(self.btnGo_clicked)
  
-  def btnPress1_clicked(self):
-    #以文本的形式输出到多行文本框
-    output = get_output(codegen.gen_code, args=(self.textEdit.toPlainText(),))
-    self.textShow.setPlainText(output)
- 
-  def btnPress2_clicked(self):
-    #以Html的格式输出多行文本框，字体红色，字号6号
-    self.textEdit.setHtml("<font color='red' size='6'><red>Hello PyQt5!\n单击按钮。</font>")
-  
-  def on_text_change(self):
-    self.textEdit.setFontFamily('consolas')
-    self.textEdit.setFontPointSize(13)
+  def btnGo_clicked(self):
+    output = get_output(codegen.gen_code, args=(self.textSrc.toPlainText(),))
+    self.textAsm.setPlainText(output)
+
+class MyWindow(QMainWindow):
+
+  def __init__(self,parent=None):
+
+    super().__init__(parent)
+    self.layout=QHBoxLayout()
+    self.menubar=self.menuBar()#获取窗体的菜单栏
+
+    self.file=self.menubar.addMenu("系统菜单")
+    self.file.addAction("New File")
+
+    self.save=QAction("Save",self)
+    self.save.setShortcut("Ctrl+S")#设置快捷键
+    self.file.addAction(self.save)
+
+    self.edit=self.file.addMenu("Edit")
+    self.edit.addAction("copy")#Edit下这是copy子项
+    self.edit.addAction("paste")#Edit下设置paste子项
+
+    self.quit=QAction("Quit",self)#注意如果改为：self.file.addMenu("Quit") 则表示该菜单下必须柚子菜单项；会有>箭头
+    self.file.addAction(self.quit)
+    self.file.triggered[QAction].connect(self.processtrigger)
+    self.setLayout(self.layout)
+    self.setWindowTitle("Menu Demo")
+    self.setCentralWidget(CompilerPage())
+
+
 
 if __name__=="__main__":
   # print(get_output(codegen.gen_code, args=('int a=1;',)))
   # quit()
   app=QApplication(sys.argv)
-  win=TextEditDemo()
+  font = QFont('consolas')
+  pointsize = font.pointSize()
+  font.setPixelSize(pointsize*1.5)
+  app.setFont(font)
+  win=CompilerPage()
   win.show()
   sys.exit(app.exec_())
